6d01dc235f8b501fa0efa58a3c78e47c607892cc
Satshabad Khalsa <satshabad.music@gmail.com>
2012-06-16 16:29:00 -0700

    moved models in here temporarily

--- src/disqusit.py
+++ src/disqusit.py
@@ -16,69 +16,63 @@ from functools import wraps
 from disqusapi import InvalidAccessToken
 from disqusapi import DisqusAPI
 
+
+from flask.ext.sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/disqusit.db'
+db = SQLAlchemy(app)
+
 ## TODO: make this read from a file
-public_key = "PUT YOUR KEY HERE"
-secret_key = "PUT YOUR KEY HERE"
+#public_key = "PUT YOUR KEY HERE"
+#secret_key = "PUT YOUR KEY HERE"
 
-app.secret_key = "this is my salt"
+#app.secret_key = "this is my salt"
 
-disqus = DisqusAPI(secret_key, public_key)
+#disqus = DisqusAPI(secret_key, public_key)
 
 
 ################################################################################
 ## UTILS
 
 
-class User(object):
-    """User object based on disqus auth token"""
-    def __init__(self, username, user_id, access_token, expires_in, token_type, refresh_token):
-        super(User, self).__init__()
-        self.username = username
-        self.user_id = user_id
-        self.access_token = access_token
-        self.expires_in = expires_in
-        self.token_type = token_type
-        self.refresh_token = refresh_token
+#class User(object):
+#    """User object based on disqus auth token"""
+#    def __init__(self, username, user_id, access_token, expires_in, token_type, refresh_token):
+#        super(User, self).__init__()
+#        self.username = username
+#        self.user_id = user_id
+#        self.access_token = access_token
+#        self.expires_in = expires_in
+#        self.token_type = token_type
+#        self.refresh_token = refresh_token
 
-    def __repr__(self):
-        return "<{username}:{access_token}>".format(**self.__dict__)
+#    def __repr__(self):
+#        return "<{username}:{access_token}>".format(**self.__dict__)
 
 
-def current_user():
-    cu = None
-    if 'auth' in session:
-        auth = session['auth']
-        cu = User(**auth)
-    return cu
+#def current_user():
+#    cu = None
+#    if 'auth' in session:
+#        auth = session['auth']
+#        cu = User(**auth)
+#    return cu
 
 
 ################################################################################
 ## APP
 
 
-@app.route("/")
-def hello():
-    cu = current_user()
-    if cu:
-        return 'Logged in as %s' % escape(session['username'])
-    else:
-        return redirect('/oauth/authorize/')
+@app.route("/news")
+def news():
+    return "the news"
 
 
-@app.route("/bye")
-def goodbye():
-    return "Goodbye World!"
-
-
-@app.route("/foo/<int:bar>/<baz>")
-def foobarbaz(bar, baz):
-    if bar == 1:
-        return "yay it's a one!"
-    elif bar == 2:
-        return "two"
-    else:
-        return baz
-
+#@app.route("/")
+##def hello():
+##    cu = current_user()
+##    if cu:
+##        return 'Logged in as %s' % escape(session['username'])
+##    else:
+##        return redirect('/oauth/authorize/')
 
 ################################################################################
 ## AUTH STUFF (DO NOT CHANGE)
@@ -153,8 +147,43 @@ def oauth_callback():
     return redirect('/')
 
 ################################################################################
+
+## MODELS ##
+
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(500))
+    shortname = db.Column(db.String(100))		##TODO Are these max lengths ok?
+    disqus_identifier = db.Column(db.String(100))
+    disqus_votes = db.Column(db.Integer)
+    disqusit_votes = db.Column(db.Integer)
+
+    def __init__(self, url=None, shortname=None, disqus_id=None,
+                 disqus_votes=0, disqusit_votes=0):
+        self.url = url
+        self.shortname = url
+        self.disqus_identifier = disqus_id
+        self.disqus_votes = disqus_votes
+        self.disqusit_votes = disqusit_votes
+
+    def __repr__(self):
+        return "<Link %r>" % (self.url)
+
+class DisqusitUser(db.Model):
+    username = db.Column(db.String(30), primary_key = True) ##Usernames are db keys
+    ##TODO Add more things
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User %r>" % (self.username)
+
 ## RUN IT
 
 if __name__ == "__main__":
     app.config["DEBUG"] = True
     app.run()
+
+
